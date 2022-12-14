beeb15f9906113a3469403b0ca511dff69a42b6b
Alexander Nemtsev <mrcrazy696@gmail.com>
2022-12-14 16:07:16 +0300

    first commit

---
+++ handler.js
@@ -0,0 +1,15 @@
+function handleSignInButton(event) {
+    event.preventDefault();
+    serializeForm(signInForm);
+}
+
+function serializeForm(formNode) {
+    const { elements } = formNode;
+    const email = elements[0]['value'];
+    const password = elements[1]['value'];
+
+    alert(`Email: ${email}\nPassword: ${password}`);
+}
+
+const signInForm = document.querySelector('.sign-in-form');
+signInForm.addEventListener('submit', handleSignInButton);
